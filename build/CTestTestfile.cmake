29fb3b4f872e26945da49a8e7651817be0e0692a
vadym2 <vadymkuku283@gmail.com>
2023-04-23 08:11:45 +0300

    shit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vadim/Desktop/tutorial
+# Build directory: /home/vadim/Desktop/tutorial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int, char**) {
+    int height;
+
+    std::cout << "Enter you height ";
+    std::cin >> height;
+    std::cout << "You height " << height << std::endl;
+}
